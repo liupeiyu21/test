ad393cb6f0a641f5d1b4971c6476339364f13dd4
Liu Peiyu <24aw0142@jec.ac.jp>
2024-12-04 18:34:38 +0900

    新たなバージョンを更新しました。

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,38 @@
 import React, { useState } from "react";
-import storeData from "./data/storeData.json"; // JSONデータをインポート
+import storeData from "./data/storeData.json"; 
 
-function App() {
-  const [selectedRegion, setSelectedRegion] = useState("Tohoku"); // 選択された地域を管理
 
-  // 選択された地域の店舗情報を取得
-  const storeList = storeData.regions[selectedRegion];
+function App(){
 
-  const handleRegionChange = (event) => {
-    setSelectedRegion(event.target.value); // 選択された地域を更新
-  };
+  const[ selectarea, setselectarea] = useState("Tohoku");
 
-  return (
+  const areaList = storeData.regions[selectarea];
+
+  return(
     <>
-      <div>
-        <h1>店舗情報</h1>
-        <label htmlFor="region-select">
-          地域を選択：
-          <select
-            name="region"
-            id="region-select"
-            value={selectedRegion}
-            onChange={handleRegionChange}
-          >
-            <option value="Tohoku">Tohoku</option>
-            <option value="Kanto">Kanto</option>
-            <option value="Kansai">Kansai</option>
-          </select>
-        </label>
-
-        <div>
-          {/* 選択された地域の店舗情報を表示 */}
-          {storeList.map((store, index) => (
-            <div key={index}>
-              <h2>{store.storeName}</h2>
-              <p>住所：{store.address}</p>
-              <p>電話番号：{store.phoneNumber}</p>
-            </div>
-          ))}
-        </div>
+    <h1>店舗紹介</h1>
+    <label htmlFor="selearea">
+      <select 
+      name="region" 
+      id="selearea" 
+      onChange={(e) =>setselectarea(e.target.value)}
+      > 
+        <option value="Tohoku">東北</option>
+        <option value="Kanto">関東</option>
+        <option value="Kansai">関西</option>
+      </select>
+    </label>
+    {areaList.map((store, index) => (
+      <div key={index}>
+        <h2>{store.storeName}</h2>
+        <p>住所：{store.address}</p>
+        <p>電話番号：{store.phoneNumber}</p>
       </div>
+    ))}
     </>
+
   );
 }
-
 export default App;
 
 
